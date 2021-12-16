e2136e5348ffdb8d687f3c2a83e49ff04a2bccfc
Marcos Eduardo <markuzemb18@gmail.com>
2021-12-16 13:08:51 -0600

    Se agregaron las intrucciones correspondientes

---
+++ src/CodersApp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const CodersApp = () => {
+
+    /*
+        1.Definir las componentes correspondientes.
+        2.Realizar una carpeta por cada componente, dentro de ella contendra el componente y el css correspondiente
+        Guardarlo como xxxx.css.
+        
+        Parte 1:
+        Maquetacion Home(Marcos), Login y Registro (Gaby)
+
+        Parte 2:
+        La componente Navbar siempre tiene que estar presente en cualquiera de las 4
+        Cuando se validen los datos de habilitaran las siguientes y se desabilitaran las 3 anteriores
+        Maquetacion Feed (Marcos), Profile, Edit Profile, Help (Gaby)
+    */
+
+    return (
+        <>
+            <div id="codersApp">
+                {/* Cargamos componente que contiene el Home,Login,Register 
+                    Si los datos se validan en Login o Registe, se renderiza el codersApp con
+                    una nuevo componente que contiene Feed(si es desde Login) o el Edit Profile(si es desde Register)
+                    Por default el nuevo componete debe la barra de navegacion y el lateral izquierdo
+                */}
+
+            </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
+import {CodersApp} from './CodersApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CodersApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
